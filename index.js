bacd4ba02a26511912b668530d638ceeaa383940
Efelu3 <fernandole1124@gmail.com>
2025-05-20 15:41:07 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Lux from "./Lux";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Lux />
+  </React.StrictMode>
+);
